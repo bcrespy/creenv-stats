446463737a0d130afb5f82a4f93570264aed6cf1
Baptiste Crespy <baptiste.crespy@gmail.com>
2018-10-21 18:21:22 +0200

    first working version

---
+++ dev/main.js
@@ -0,0 +1,28 @@
+import Stats from '../lib/index';
+
+let stats = new Stats();
+
+
+let cvs = document.createElement("canvas");
+let ctx = cvs.getContext("2d");
+
+cvs.width = 800;
+cvs.height = 800;
+
+document.body.appendChild(cvs);
+
+let i = 0;
+
+function update () {
+  window.requestAnimationFrame(update);
+
+  stats.begin();
+  ctx.clearRect(0,0,800,800);
+  i++;
+  ctx.fillStyle = "blue";
+  ctx.fillRect(Math.cos(i/100)*300+300, Math.sin(i/100)*300+300, 50, 50);
+  ctx.fillRect(Math.cos(i/100 - Math.PI)*300+300, Math.sin(i/100-Math.PI)*300+300, 50, 50);
+  stats.end();
+}
+
+update();
